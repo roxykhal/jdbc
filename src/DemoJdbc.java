3ab9d9a2c43d4c60a80b1fc7fefb3f19cfc4bd88
Roxy Khalilifar <roxykhal@hotmail.com>
2025-02-13 17:40:42 +0000

    import package and load and register

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -2,22 +2,28 @@ import java.sql.*;
 
 public class DemoJdbc {
 
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws Exception {
 
         /*
         import package
         load and register
-        create connection
+        create connection - connect application to DB
         create statement
         execute statement
         process results
         close
          */
 
+        String url = "jdbc:postgresql://localhost:5432/demo";
+        String uname = "postgres";
+        String pass = "Ladysmithroad123";
+
+
         //load and register
         Class.forName("org.postgresql.Driver");
 
         //create connection
+        Connection con = DriverManager.getConnection(url,uname,pass);
 
 
 
