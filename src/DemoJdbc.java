cc8241d96298cba969f394bc36402ca64da3340b
Roxy Khalilifar <roxykhal@hotmail.com>
2025-02-13 17:17:35 +0000

    Initial commit

---
+++ src/DemoJdbc.java
@@ -0,0 +1,19 @@
+public class DemoJdbc {
+
+    public static void main(String[] args) {
+
+        /*
+        import package
+        load and register
+        create connection
+        create statement
+        execute statement
+        process results
+        close
+         */
+
+
+
+
+    }
+}
